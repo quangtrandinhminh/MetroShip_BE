79d41095058553ac655bea062daf120cb115939c
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2025-05-16 13:00:28 +0700

    Update Program.cs

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -7,7 +7,7 @@ using AppDbContext = MetroShip.Repository.Infrastructure.AppDbContext;
 var builder = WebApplication.CreateBuilder(args);
 
 // Load environment variables from root folder
-Env.Load("../");
+Env.Load("../be.env");
 string appName = Environment.GetEnvironmentVariable("APP_NAME");
 Console.WriteLine($"Environment: {appName}");
 
