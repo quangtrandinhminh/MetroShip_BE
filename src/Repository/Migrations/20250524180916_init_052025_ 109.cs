91fbf434f2963959862405df65a007c675cccceb
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2025-05-26 17:39:27 +0700

    fix itinerary entity

---
+++ src/Repository/Migrations/20250524180916_init_052025_ 109.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MetroShip.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class init_052025_109 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "BasePriceVndPerKm",
+                table: "ShipmentItineraries",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BasePriceVndPerKm",
+                table: "MetroLines",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BasePriceVndPerKm",
+                table: "ShipmentItineraries");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BasePriceVndPerKm",
+                table: "MetroLines",
+                type: "numeric(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)");
+        }
+    }
+}
