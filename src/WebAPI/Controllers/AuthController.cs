500d7d932369fa9cb37f419bc809f5c0cd1684b2
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2025-06-04 23:08:59 +0700

    Update AuthController.cs

--- src/WebAPI/Controllers/AuthController.cs
+++ src/WebAPI/Controllers/AuthController.cs
@@ -16,11 +16,15 @@ namespace MetroShip.WebAPI.Controllers
     {
         private readonly IAuthService _authService;
         private readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(IAuthService authSevices, IUserService userService)
+        public AuthController(IAuthService authSevices,
+            IUserService userService,
+            IConfiguration configuration)
         {
             _userService = userService;
             _authService = authSevices;
+            _configuration = configuration;
         }
 
         [HttpGet(WebApiEndpoint.Authentication.Hello)]
