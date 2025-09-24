b729c1a30ea7f89350a5a4b5d1c1ac8037ec2290
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2025-09-25 00:39:30 +0700

    Update SystemSettingModel.cs

--- src/Utility/Config/SystemSettingModel.cs
+++ src/Utility/Config/SystemSettingModel.cs
@@ -112,8 +112,7 @@ namespace MetroShip.Utility.Config
         public string MAX_CAPACITY_PER_LINE_KG { get; set; }
         public string MAX_CAPACITY_PER_LINE_M3 { get; set; }
         public string MAX_NUMBER_OF_SHIFT_ATTEMPTS { get; set; }
-
-
+        public string NUM_OF_MAX_DISTANCE_EXPANSION_TIMES { get; set; }
 
         // main policy
         public string MAX_SCHEDULE_SHIPMENT_DAY { get; set; }
