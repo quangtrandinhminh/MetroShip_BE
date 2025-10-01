c84352873852a445144ab97afe2f11b175148de8
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2025-10-02 01:33:24 +0700

    Update SupportTicketService.cs

--- src/Service/Services/SupportTicketService.cs
+++ src/Service/Services/SupportTicketService.cs
@@ -14,6 +14,7 @@ using MetroShip.Utility.Helpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
+using System.Linq;
 
 namespace MetroShip.Service.Services;
 
@@ -72,12 +73,14 @@ public class SupportTicketService(IServiceProvider serviceProvider) : ISupportTi
         var stationId = JwtClaimUltils.GetUserStation(_httpContextAccessor);
         var role = JwtClaimUltils.GetUserRole(_httpContextAccessor);
         
-        if (stationId != null)
+        if (role.Contains(UserRoleEnum.Staff.ToString()) && stationId != null)
         {
             _logger.Information("User is associated with station {StationId}, filtering tickets by station", stationId);
             tickets = await _supportingTicketRepository.GetAllPaginatedQueryable(
                 request.PageNumber,
                 request.PageSize,
+                // tickets where the shipment is departing from the user's station and is not a return shipment
+                // or tickets where the shipment is arriving at the user's station and is a return shipment
                 t => (t.Shipment.DepartureStationId == stationId && t.Shipment.ReturnForShipmentId == null)
                 || (t.Shipment.DestinationStationId == stationId && t.Shipment.ReturnForShipmentId != null)
                 && t.DeletedAt == null);
@@ -91,13 +94,17 @@ public class SupportTicketService(IServiceProvider serviceProvider) : ISupportTi
             request.PageSize,
             t => t.OpenById == userId && t.DeletedAt == null);
         }
-        else
+        else if (role.Contains(UserRoleEnum.Admin.ToString()))
         {
-            _logger.Information("User is an admin or has no station association, fetching all tickets");
+            _logger.Information("User is an admin, fetching all tickets");
             tickets = await _supportingTicketRepository.GetAllPaginatedQueryable(
             request.PageNumber,
             request.PageSize,
             t => t.DeletedAt == null);
+        }
+        else
+        {
+            tickets = new();
         }
 
         return _mapper.MapToSupportTicketPaginatedList(tickets);
